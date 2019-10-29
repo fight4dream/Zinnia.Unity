aaefc6dbb5f52afc71d4510ebf856473efe4f7cc
Ethan Cheung <ethan@fight4dream.com>
2019-10-29 12:47:36 +0800

    smoothing

---
+++ Runtime/Data/Type/Transformation/PositionSmoother.cs
@@ -0,0 +1,42 @@
+﻿namespace Zinnia.Data.Type.Transformation
+{
+    using UnityEngine;
+    using UnityEngine.Events;
+    using System;
+    using Malimbe.PropertySerializationAttribute;
+    using Malimbe.XmlDocumentationAttribute;
+
+    public class PositionSmoother : Transformer<Vector3, Vector3, PositionSmoother.UnityEvent>
+    {
+        /// <summary>
+        /// Defines the event with the <see cref="Vector3"/> smoothed.
+        /// </summary>
+        [Serializable]
+        public class UnityEvent : UnityEvent<Vector3>
+        {
+        }
+
+        /// <summary>
+        /// The maximum allowed distance between the unsmoothed source and the smoothed target per frame to use for smoothing.
+        /// </summary>
+        [Serialized]
+        [field: DocumentedByXml, Range(0.0001f, 100f)]
+        public float MaxAllowedPerFrameDistanceDifference { get; set; } = 0.003f;
+
+        /// <summary>
+        /// The position that results by following source.
+        /// </summary>
+        protected Vector3 targetPosition;
+
+        /// <summary>
+        /// Smooth the given <see cref="Vector3"/>.
+        /// </summary>
+        /// <param name="input">The value to smooth.</param>
+        /// <returns>A new smoothed <see cref="Vector3"/>.</returns>
+        protected override Vector3 Process(Vector3 input)
+        {
+            float alpha = Mathf.Clamp01(Vector3.Distance(targetPosition, input) / MaxAllowedPerFrameDistanceDifference);
+            return Vector3.Lerp(targetPosition, input, alpha);
+        }
+    }
+}
---
+++ Runtime/Data/Type/Transformation/VelocitySmoother.cs
@@ -0,0 +1,42 @@
+﻿namespace Zinnia.Data.Type.Transformation
+{
+    using UnityEngine;
+    using UnityEngine.Events;
+    using System;
+    using Malimbe.PropertySerializationAttribute;
+    using Malimbe.XmlDocumentationAttribute;
+
+    public class VelocitySmoother : Transformer<Vector3, Vector3, VelocitySmoother.UnityEvent>
+    {
+        /// <summary>
+        /// Defines the event with the <see cref="Vector3"/> smoothed.
+        /// </summary>
+        [Serializable]
+        public class UnityEvent : UnityEvent<Vector3>
+        {
+        }
+
+        /// <summary>
+        /// The maximum allowed magnitude difference between the unsmoothed source and the smoothed target per frame to use for smoothing.
+        /// </summary>
+        [Serialized]
+        [field: DocumentedByXml, Range(0.0001f, 100f)]
+        public float MaxAllowedPerFrameVelocityDifference { get; set; } = 0.003f;
+
+        /// <summary>
+        /// The velocity that results by following source.
+        /// </summary>
+        protected Vector3 targetVelocity;
+
+        /// <summary>
+        /// Smooth the given <see cref="Vector3"/>.
+        /// </summary>
+        /// <param name="input">The value to smooth.</param>
+        /// <returns>A new smoothed <see cref="Vector3"/>.</returns>
+        protected override Vector3 Process(Vector3 input)
+        {
+            float alpha = Mathf.Clamp01(Mathf.Abs(targetVelocity.magnitude - input.magnitude) / MaxAllowedPerFrameVelocityDifference);
+            return Vector3.Lerp(targetVelocity, input, alpha);
+        }
+    }
+}
